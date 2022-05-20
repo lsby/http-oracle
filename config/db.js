80a6a0a8a75be0491fa50ba50097d483faa72cf5
hbybyyang <2451759073@qq.com>
2022-05-21 05:43:03 +0800

    start

---
+++ config/db.js
@@ -0,0 +1,8 @@
+export default {
+    test: {
+        user: 'system',
+        password: 'oracle',
+        connectString: '127.0.0.1:1521/EE.oracle.docker',
+        // connectString: 'host.docker.internal:1521/EE.oracle.docker',
+    },
+}
